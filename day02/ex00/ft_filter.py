a8435e86b096dc7d0a13054ecdfbe08bd127acc3
Taeheon-Lee <taeheon714@gmail.com>
2020-06-06 22:46:58 +0900

    finish ft_filter.py

--- day02/ex00/ft_filter.py
+++ day02/ex00/ft_filter.py
@@ -1,2 +1,6 @@
 def ft_filter(function_to_apply, list_of_inputs):
-    pass
+    result = []
+    for elem in list_of_inputs:
+        if function_to_apply(elem) == 1:
+            result.append(elem)
+    return (result)
