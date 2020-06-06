25d50fefcd0079e89ab7f109d97ae4b9718e3c38
Taeheon-Lee <taeheon714@gmail.com>
2020-06-06 23:59:01 +0900

    add condition

--- day02/ex00/ft_filter.py
+++ day02/ex00/ft_filter.py
@@ -1,6 +1,10 @@
 def ft_filter(function_to_apply, list_of_inputs):
+    if not isinstance(list_of_inputs, list):
+        raise TypeError(str(type(list_of_inputs))[7:-1:] + " object is not callable")
+    if not (str(type(function_to_apply)) == "<class 'function'>"):
+        raise TypeError(str(type(function_to_apply))[7:-1:] + " object is not callable")
     result = []
     for elem in list_of_inputs:
-        if function_to_apply(elem) == 1:
+        if function_to_apply(elem) == True:
             result.append(elem)
     return (result)
--- day02/ex00/ft_map.py
+++ day02/ex00/ft_map.py
@@ -1,4 +1,8 @@
 def ft_map(function_to_apply, list_of_inputs):
+    if not isinstance(list_of_inputs, list):
+        raise TypeError(str(type(list_of_inputs))[7:-1:] + " object is not callable")
+    if not (str(type(function_to_apply)) == "<class 'function'>"):
+        raise TypeError(str(type(function_to_apply))[7:-1:] + " object is not callable")
     result = []
     for elem in list_of_inputs:
         result.append(function_to_apply(elem))
