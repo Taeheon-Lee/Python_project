6f7e8507b6324d8119c279f8c76924688b658226
Taeheon-Lee <taeheon714@gmail.com>
2020-06-10 23:54:10 +0900

    finish logger.py

--- day02/ex02/logger.py
+++ day02/ex02/logger.py
@@ -1,5 +1,22 @@
 import time
 from random import randint
+import logging
+import functools
+
+def log(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kargs):
+        start_time = time.perf_counter()
+        value = func(*args, **kargs)
+        end_time = time.perf_counter()
+        run_time = end_time - start_time
+        if run_time < 1.0:
+            run_time *= 1000
+            logger.info(f"{func.__name__.title().replace('_', ' ')}\t[ exec-time =  {run_time:.3f} ms ]")
+        else:
+            logger.info(f"{func.__name__.title().replace('_', ' ')}\t[ exec-time =  {run_time:.3f} s ]")
+        return (value)
+    return wrapper
 
 class CoffeeMachine():
 
@@ -32,10 +49,17 @@ class CoffeeMachine():
         self.water_level += water_level
         print("Blub blub blub...")
 
-    if __name__ == "__main__":
-        machine = CoffeeMachine()
-        for i in range(0, 5):
-            machine.make_coffee()
+if __name__ == "__main__":
+    logger = logging.getLogger("cmaxime")
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('(%(name)s)Running: %(message)s')
+    file_handler = logging.FileHandler('machine.log')
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
 
+    machine = CoffeeMachine()
+    for i in range(0, 5):
         machine.make_coffee()
-        machine.add_water(70)
+    machine.make_coffee()
+    machine.add_water(70)
